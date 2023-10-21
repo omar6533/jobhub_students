08719ee4a6dc10f7e44da106dd44e7871e28f17c
OMAR ALSAFADI <omar.h.alsafadi@gmail.com>
2023-10-21 23:26:36 +0300

    add category section

--- lib/student_landing/controller/student_landing_controller.dart
+++ lib/student_landing/controller/student_landing_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 class StudentLandingController extends GetxController {
   RxInt selectedIndex = 0.obs;
+  RxInt selectedCategory = (-1).obs;
 
   selectNavigationIndex(int index) {
     selectedIndex.value = index;
--- lib/student_landing/views/jobs_landing_view.dart
+++ lib/student_landing/views/jobs_landing_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:get/get.dart';
 import 'package:jobhub/authintication/controllers/auth_controller.dart';
+import 'package:jobhub/constants.dart';
 import 'package:jobhub/shared/components/jobHub_text.dart';
+import 'package:jobhub/student_landing/controller/student_landing_controller.dart';
 
 class JobsLandingView extends StatelessWidget {
   const JobsLandingView({super.key});
@@ -11,11 +13,13 @@ class JobsLandingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AuthController authController = Get.find();
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView(
-        children: [
-          Row(
+    StudentLandingController studentLandingController = Get.find();
+    return ListView(
+      children: [
+        // image section
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ClipOval(
@@ -40,9 +44,45 @@ class JobsLandingView extends StatelessWidget {
                 ],
               )
             ],
-          )
-        ],
-      ),
+          ),
+        ), //end of image section
+        SizedBox(
+          height: 5.h,
+        ),
+        SizedBox(
+          height: 5.h,
+          child: ListView.builder(
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  if (studentLandingController.selectedCategory.value ==
+                      index) {
+                    studentLandingController.selectedCategory.value = -1;
+                  } else {
+                    studentLandingController.selectedCategory.value = index;
+                  }
+                },
+                child: Obx(
+                  () => Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        color:
+                            studentLandingController.selectedCategory == index
+                                ? AppColors.primary
+                                : Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: const EdgeInsets.all(8),
+                    margin: EdgeInsets.symmetric(horizontal: 2.w),
+                    child: JobHubText(text: 'category'),
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
