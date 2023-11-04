e26c69c9d24eb27f7b2fddef041cffcb7cfd58b7
OMAR ALSAFADI <omar.h.alsafadi@gmail.com>
2023-11-04 22:10:56 +0300

    add the collection and firestor edepcndet

--- lib/profile/controller/studnet_profile_controller.dart
+++ lib/profile/controller/studnet_profile_controller.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class StudentProfileController extends GetxController {
+  CollectionReference studentsCollection =
+      FirebaseFirestore.instance.collection("students ");
+
+  addStudent() {}
+
   RxBool isStudentProfileEmpty = true.obs;
 }
